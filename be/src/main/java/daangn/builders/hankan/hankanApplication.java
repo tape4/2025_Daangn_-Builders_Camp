067a66a0b62cf6ea27f60738435a7651a4b9d544
JeongHoon Lee <sherry0_@kakao.com>
2025-09-20 21:45:53 +0900

    feat: enable JPA Auditing for entity timestamps
    - Add @EnableJpaAuditing to main application class
    - Enables automatic createdAt/updatedAt field management

--- be/src/main/java/daangn/builders/hankan/hankanApplication.java
+++ be/src/main/java/daangn/builders/hankan/hankanApplication.java
@@ -6,9 +6,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.env.Environment;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @Slf4j
 @SpringBootApplication
+@EnableJpaAuditing
 public class hankanApplication {
 
 	public static void main(String[] args) {
