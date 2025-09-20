343a6332e45e941c6bcee34e4dfd5442c9a8ca0d
JeongHoon Lee <sherry0_@kakao.com>
2025-09-20 23:48:55 +0900

    fix: JSON 직렬화 일관성을 위한 BoxCapacityResponse 개선
    - @JsonProperty 어노테이션 추가로 camelCase 필드명 보장
    - Jackson 직렬화 시 sCount, mCount, lCount 등이 올바르게 출력되도록 수정
    - API 응답의 JSON 스키마 일관성 확보

--- be/src/main/java/daangn/builders/hankan/api/dto/BoxCapacityResponse.java
+++ be/src/main/java/daangn/builders/hankan/api/dto/BoxCapacityResponse.java
@@ -1,15 +1,27 @@
 package daangn.builders.hankan.api.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Builder;
 import lombok.Getter;
 
 @Getter
 @Builder
 public class BoxCapacityResponse {
+    @JsonProperty("xsCount")
     private int xsCount;
+    
+    @JsonProperty("sCount")
     private int sCount;
+    
+    @JsonProperty("mCount")
     private int mCount;
+    
+    @JsonProperty("lCount")
     private int lCount;
+    
+    @JsonProperty("xlCount")
     private int xlCount;
+    
+    @JsonProperty("totalCount")
     private int totalCount;
 }
