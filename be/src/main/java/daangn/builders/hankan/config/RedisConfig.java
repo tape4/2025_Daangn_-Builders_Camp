5c190c76278035b0741d514e289f3b98da189148
JeongHoon Lee <sherry0_@kakao.com>
2025-09-21 04:57:21 +0900

    backtrace

--- be/src/main/java/daangn/builders/hankan/config/RedisConfig.java
+++ be/src/main/java/daangn/builders/hankan/config/RedisConfig.java
@@ -11,10 +11,10 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 @Configuration
 public class RedisConfig {
 
-    @Value("${spring.redis.host:localhost}")
+    @Value("${spring.data.redis.host:localhost}")
     private String redisHost;
 
-    @Value("${spring.redis.port:6379}")
+    @Value("${spring.data.redis.port:6379}")
     private int redisPort;
 
     @Bean
